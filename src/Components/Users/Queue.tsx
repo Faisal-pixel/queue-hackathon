bcc6dbf94b474e5fb9835737d86ee73fe7465934
Fiopefoluwa <iamfope2006@gmail.com>
2024-09-12 21:35:24 +0100

    CSS done

--- src/Components/Users/Queue.tsx
+++ src/Components/Users/Queue.tsx
@@ -46,11 +46,11 @@ const Userface: React.FC = () => {
 
     return (
         <div className="p-4 bg-orange-100 h-screen">
-            <h1 className="text-3xl font-bold mb-4 text-center">Queue Who's Up Next</h1>
+            <h1 className="text-3xl font-bold text-center">Who's Up Next?</h1>
             {tickets.length === 0 ? (
                 <p className="text-center text-gray-500 pt-60 text-2xl">Nobody on the QUEUE yet</p>
             ) : (
-                <table className="w-full border-collapse">
+                <table className=" translate-y-6 w-full border-collapse">
                     <thead>
                         <tr>
                             <th className="border px-4 py-2 text-center">#</th>
